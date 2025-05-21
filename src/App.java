c0f972ee353b7769be5461238be6fc84cf535d69
ALVARO <alvaroch975@gmail.com>
2025-05-21 13:11:19 +0200

    MODIFICADO

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Holis, soy Lucia :3");
+        System.out.println("Holis, soy Alvaro ");
+        
+        int i = 8;
+        System.out.println(i);
     }
 }
