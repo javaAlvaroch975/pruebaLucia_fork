f1b3cf71caa185e0cc003cbd660b762d0725fe7b
ALVARO <alvaroch975@gmail.com>
2025-05-21 13:31:27 +0200

    develop2

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Holis, soy Lucia :3");
-        System.out.println("Holis, soy Alvaro ");
-        
-        int i = 15;
+        System.out.println("Holis, soy Alvaro ");      
+        int i = 32;
         System.out.println(i);
+
+        int x = i -5;
+
+        System.out.println(x);
     }
 }
