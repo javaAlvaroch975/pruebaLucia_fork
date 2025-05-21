45f16e67e324f6d8b06f2b25af3b14799195bf4c
ALVARO <alvaroch975@gmail.com>
2025-05-21 13:29:19 +0200

    MAIN

--- src/App.java
+++ src/App.java
@@ -2,8 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Holis, soy Lucia :3");
         System.out.println("Holis, soy Alvaro ");
+        System.out.println("nuevo cambio");
         
-        int i = 15;
+        int i = 20;
         System.out.println(i);
     }
 }
